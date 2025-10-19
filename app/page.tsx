7768cd9008b5fa2e9bfeeacc7a426078d026c159
adeTunes <babatundeadebowale1998@gmail.com>
2025-10-19 14:12:04 +0100

    fix: header style change

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <Logo className="sm:w-[200px] w-[121px]" />
       </nav>
       <div className="flex flex-col sm:gap-4 gap-[10px]">
-        <header className="sm:py-10 max-w-[1344px] px-4 pt-4">
+        <header className="sm:py-10 mx-auto w-full max-w-[1344px] px-4 pt-4">
           <h1 className="md:text-5xl uppercase text-2xl font-medium">News</h1>
         </header>
         <NewsList />
